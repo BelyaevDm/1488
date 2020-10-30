79d635ff0d0579b0550e80bbd9a5f972f8533b5a
BelyaevDm <dimabelyaev_2002@mail.ru>
2020-10-30 21:06:59 +0500

    CheckInput

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -4,6 +4,37 @@ namespace Chess
 {
     class Program
     {
+        public static void CheckInput()
+        {
+            Console.Write("Введите начальную координату: ");
+            string aStartPlace = Console.ReadLine();
+            Console.Write("Введите конченую координату: ");
+            string aFinishPlace = Console.ReadLine();
+            bool aErrors = false;
+            if (aStartPlace.Length !=2 || aFinishPlace.Length != 2)
+            {
+                aErrors = true;
+            }
+            else
+            {
+                for (int i = 0; i < aStartPlace.Length; i++)
+                {
+                    if (aStartPlace[0] > 'H' || aStartPlace[0] < 'A' || aFinishPlace[0] > 'H' || aFinishPlace[0] < 'A')
+                    {
+                        aErrors = true;
+                    }
+                    if (aStartPlace[1] > 8 || aStartPlace[1] < 1 || aFinishPlace[1] > 8 || aFinishPlace [1] < 1)
+                    {
+                        aErrors = true;
+                    }
+                }
+            }
+            if (aErrors)
+            {
+                Console.WriteLine("Ошибка в координатах");
+            }
+        }
+
         static void Main(string[] args)
         {
             char[] board; 
